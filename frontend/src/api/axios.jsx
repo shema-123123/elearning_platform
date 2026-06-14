d3f7eb4528bb8447ee2f5c743b358b24d4979712
shema <shemapaccj@gmail.com>
2026-06-14 15:17:31 +0200

    Update axios.jsx

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // otherwise it uses the hardcoded Back4app link.
-  baseURL:  'https://stock-16wbdco1.b4a.run/',
+  baseURL:  'https://stock-qm20yubg.b4a.run/',
   headers: {
     'Content-Type': 'application/json',
   }
