2e70fe9cd3d735315681d1ab50ac1d20ee0f2f2d
shema <shemapaccj@gmail.com>
2026-06-14 08:54:32 +0200

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet, Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../Contexts/AuthContext'
 import {
   HomeIcon,
   PackageIcon,
