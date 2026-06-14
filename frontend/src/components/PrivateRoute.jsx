e8b59f9898fefafd0a044ec7067e56955a74c700
shema <shemapaccj@gmail.com>
2026-06-14 08:51:36 +0200

    Update PrivateRoute.jsx

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../Contexts/AuthContext'
 const PrivateRoute = ({ children }) => {  
     const { user, loading } = useAuth()    
     if (loading) {    
